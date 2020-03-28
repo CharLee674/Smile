251820f4c5386bcef2eae6b1a44fe00946796603
dong- <dong-jane@hotmail.com>
2020-03-28 14:09:25 -0700

    tried to embed graphics

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Janes fantasy kindom' });
 });
 
 module.exports = router;
