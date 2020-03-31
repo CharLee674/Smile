f5557a40451495c673401f11ce59bfbff665a7b1
farrandi <farrandi.nando@alumni.ubc.ca>
2020-03-31 09:34:34 -0700

    minor changes
    Signed-off-by: farrandi <farrandi.nando@alumni.ubc.ca>

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var app = express();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Smile' });
 });
+
+_dirname = process.cwd();
+app.use(express.static(_dirname + '/public'))
 
 module.exports = router;
